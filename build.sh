070c98a4c12086e284a781071b142b6009c02bbf
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2024-09-18 21:15:50 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,11 +13,13 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen
+#rpm-ostree install screen
+rpm-ostree install webkit2gtk4-0
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
 
-#### Example for enabling a System Unit File
+# this would install a package from flathub
+flatpak install flathub it.mijorus.gearlever
 
 systemctl enable podman.socket
